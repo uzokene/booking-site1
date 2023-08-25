fc0ab1bb9fc092931bc4c0e452a76ca228d10cd1
Blessing <okeneuz@gmail.com>
2023-08-25 01:50:36 +0100

    Add html files

---
+++ script.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.review-slider', {
+    spaceBetween: 20,
+    direction: 'vertical',
+    loop: true,
+    autoplay{
+        delay: 2500;
+        disableOnInteraction: false,
+    }
+});
